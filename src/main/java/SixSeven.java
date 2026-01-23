0af0328de929ce6f04f3e45d4395751312e89f88
aldenngsk <aldenngsk@gmail.com>
2026-01-23 13:44:25 +0800

    Level-6

--- src/main/java/SixSeven.java
+++ src/main/java/SixSeven.java
@@ -1,11 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SixSeven {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        Task[] tasks = new Task[100];
-        int taskCount = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
 
         System.out.println("Hello! I'm SixSeven");
         System.out.println("What can I do for you?");
@@ -21,31 +21,43 @@ public class SixSeven {
 
                 if (input.equals("list")) {
                     System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < taskCount; i++) {
-                        System.out.println((i + 1) + "." + tasks[i]);
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + "." + tasks.get(i));
                     }
                     continue;
                 }
 
                 if (input.startsWith("mark ")) {
                     int index = Integer.parseInt(input.substring(5)) - 1;
-                    if (index < 0 || index >= taskCount) {
+                    if (index < 0 || index >= tasks.size()) {
                         throw new DukeException("That task number does not exist.");
                     }
-                    tasks[index].markDone();
+                    tasks.get(index).markDone();
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(tasks[index]);
+                    System.out.println(tasks.get(index));
                     continue;
                 }
 
                 if (input.startsWith("unmark ")) {
                     int index = Integer.parseInt(input.substring(7)) - 1;
-                    if (index < 0 || index >= taskCount) {
+                    if (index < 0 || index >= tasks.size()) {
                         throw new DukeException("That task number does not exist.");
                     }
-                    tasks[index].markUndone();
+                    tasks.get(index).markUndone();
                     System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println(tasks[index]);
+                    System.out.println(tasks.get(index));
+                    continue;
+                }
+
+                if (input.startsWith("delete ")) {
+                    int index = Integer.parseInt(input.substring(7)) - 1;
+                    if (index < 0 || index >= tasks.size()) {
+                        throw new DukeException("That task number does not exist.");
+                    }
+                    Task removed = tasks.remove(index);
+                    System.out.println("Noted. I've removed this task:");
+                    System.out.println(removed);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     continue;
                 }
 
@@ -58,10 +70,10 @@ public class SixSeven {
                     if (description.isEmpty()) {
                         throw new DukeException("The description of a todo cannot be empty.");
                     }
-                    tasks[taskCount++] = new Todo(description);
+                    tasks.add(new Todo(description));
                     System.out.println("Got it. I've added this task:");
-                    System.out.println(tasks[taskCount - 1]);
-                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println(tasks.get(tasks.size() - 1));
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     continue;
                 }
 
@@ -70,10 +82,10 @@ public class SixSeven {
                     if (parts.length < 2) {
                         throw new DukeException("Please specify a deadline using /by.");
                     }
-                    tasks[taskCount++] = new Deadline(parts[0], parts[1]);
+                    tasks.add(new Deadline(parts[0], parts[1]));
                     System.out.println("Got it. I've added this task:");
-                    System.out.println(tasks[taskCount - 1]);
-                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println(tasks.get(tasks.size() - 1));
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     continue;
                 }
 
@@ -82,10 +94,10 @@ public class SixSeven {
                     if (parts.length < 3) {
                         throw new DukeException("Please specify an event using /from and /to.");
                     }
-                    tasks[taskCount++] = new Event(parts[0], parts[1], parts[2]);
+                    tasks.add(new Event(parts[0], parts[1], parts[2]));
                     System.out.println("Got it. I've added this task:");
-                    System.out.println(tasks[taskCount - 1]);
-                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println(tasks.get(tasks.size() - 1));
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     continue;
                 }
 
