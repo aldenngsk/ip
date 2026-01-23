8f09c72b2c73333c6ef61c230f8aa2661552dc44
aldenngsk <aldenngsk@gmail.com>
2026-01-23 12:58:20 +0800

    Level-1

--- src/main/java/SixSeven.java
+++ src/main/java/SixSeven.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class SixSeven {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm SixSeven");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(input);
+        }
+
+        scanner.close();
     }
 }
