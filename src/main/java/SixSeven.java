1ed0a9f0e4931e878123f087b9f35762630963f2
aldenngsk <aldenngsk@gmail.com>
2026-01-23 13:06:35 +0800

    Level-3

--- src/main/java/SixSeven.java
+++ src/main/java/SixSeven.java
@@ -5,7 +5,7 @@ public class SixSeven {
 
         Scanner scanner = new Scanner(System.in);
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         System.out.println("Hello! I'm SixSeven");
@@ -20,17 +20,34 @@ public class SixSeven {
             }
 
             if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
-            } else {
-                tasks[taskCount] = input;
-                taskCount++;
-                System.out.println("added: " + input);
+                continue;
             }
+
+            if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                tasks[index].markDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks[index]);
+                continue;
+            }
+
+            if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                tasks[index].markUndone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(tasks[index]);
+                continue;
+            }
+
+            tasks[taskCount] = new Task(input);
+            taskCount++;
+            System.out.println("added: " + input);
         }
 
         scanner.close();
     }
 }
-
---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    public void markUndone() {
+        isDone = false;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public String getStatusIcon() {
+        return isDone ? "[X]" : "[ ]";
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return getStatusIcon() + " " + description;
+    }
+}
