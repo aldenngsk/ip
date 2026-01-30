fee395f5488d4b91407ae592eb831bd4c67ae0cd
aldenngsk <aldenngsk@gmail.com>
2026-01-30 11:17:36 +0800

    Add Level-7: save and load tasks from data/duke.txt

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -7,4 +7,12 @@ public class Todo extends Task {
     protected String getTypeIcon() {
         return "[T]";
     }
+
+    /**
+     * Returns a string representation for saving to file.
+     * Format: T | 0/1 | description
+     */
+    public String toFileString() {
+        return "T | " + (isDone ? "1" : "0") + " | " + description;
+    }
 }
